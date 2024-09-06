ce354b0138624ecca635dc1a60f7ec038455f118
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-06 15:10:06 +0300

    Update main.gd

--- main.gd
+++ main.gd
@@ -15,12 +15,23 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	
+	$Music.stop()
+	$DeathSound.play()
 	
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+
+	get_tree().call_group("mobs", "queue_free")
+	
+	$Music.play()
 
 
 func _on_mob_timer_timeout() -> void:
@@ -44,6 +55,9 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	
+	$HUD.update_score(score)
+
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
